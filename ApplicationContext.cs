ca222c675580714a1a5954a9d11a1d8e1f555b80
Alexey <boronin_1993@mail.ru>
2023-11-12 13:48:02 +0300

    Добавьте файлы проекта.

---
+++ ApplicationContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ConsoleApp50
+{
+    public class ApplicationContext:DbContext
+    {
+        public DbSet<Human>Humans=>Set<Human>();
+        public ApplicationContext()=>Database.EnsureCreated();
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=helloapp.db");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using ConsoleApp50;
+
+using(ApplicationContext db=new ApplicationContext())
+{
+    Human ted = new Human { Name = "Ted", Age = 33 };
+    Human jane = new Human { Name = "Jane", Age = 25 };
+
+    db.Humans.Add(ted);
+    db.Humans.Add(jane);
+    db.SaveChanges();
+    Console.WriteLine("Объекты успешно сохранены");
+
+    var humans= db.Humans.ToList();
+    Console.WriteLine("Список объектов:");
+    foreach(Human h in humans)
+     Console.WriteLine($"{h.Id}.{h.Name} - {h.Age}");
+}
+using (ApplicationContext db=new ApplicationContext())
+{
+    Human? human=db.Humans.FirstOrDefault();
+    if(human!=null)
+    {
+        human.Name = "Bred";
+        human.Age = 25;
+        db.SaveChanges() ;
+    }
+    var humans = db.Humans.ToList();
+    Console.WriteLine("Список объектов:");
+    foreach (Human h in humans)
+        Console.WriteLine($"{h.Id}.{h.Name} - {h.Age}");
+}
+using(ApplicationContext db=new ApplicationContext())
+{
+    Human? human = db.Humans.FirstOrDefault();
+    if(human!=null)
+    {
+        db.Humans.Remove(human);
+        db.SaveChanges();
+    }
+    var humans = db.Humans.ToList();
+    Console.WriteLine("Список объектов:");
+    foreach (Human h in humans)
+        Console.WriteLine($"{h.Id}.{h.Name} - {h.Age}");
+}
